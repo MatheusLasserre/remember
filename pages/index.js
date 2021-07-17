845c496dacf17a18e6d4aa1b6ebd13e70756dde5
Matheus Lasserre <matheuslasserre3@gmail.com>
2021-07-17 00:21:51 -0300

    Renomeações de arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/home/navbar';
-import Reminder from '../components/reminder/reminders';
+import Reminder from '../components/reminder/reminder';
 import Topbar from '../components/home/topbar';
 
 export default function Home() {
